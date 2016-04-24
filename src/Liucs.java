7e81d1cbf5b8ab43420cab93a281621149537afa
cerliu <liu.c.song@gmail.com>
2016-04-24 15:44:30 +0800

    rebase

--- src/Liucs.java
+++ src/Liucs.java
@@ -12,10 +12,11 @@ public class Liucs {
 		System.out.println(c);
 		int bss = 123;
 		int k = 13;
-		int cdsd = 123123123;
+		int cdsd = 123;
 	}
 	@Test
 	public void test02(){
 		System.out.println("aaa");
+		System.out.println("43434");
 	}
 }
