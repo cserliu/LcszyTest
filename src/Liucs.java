1a126ff0533647a035b1b79b2eb83703fa3bb4a1
cerliu <liu.c.song@gmail.com>
2016-04-24 14:27:15 +0800

    change function body

--- src/Liucs.java
+++ src/Liucs.java
@@ -5,6 +5,10 @@ import org.junit.Test;
 public class Liucs {
 	@Test
 	public void test01(){
+		int a = 1;
+		int b = 2;
+		int c = a + b;
+		System.out.println(c);
 	}
 	@Test
 	public void test02(){
